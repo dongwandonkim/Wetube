f8a500227cbd1c55325c465be6dbe6bbb58f3fd7
Dongwan Kim <brkpar@gmail.com>
2020-05-20 02:18:28 -0700

    basic router setup

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import morgan from  "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter"
+import globalRouter from "./routers/globalRouter"
 
 const app = express();
 
@@ -14,8 +17,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"))
 
-app.get('/', function (req, res){
-   res.send('hello world');
-});
 
-app.listen(3000, console.log('servers running on port 3000'));
+//Routers
+app.use("/", globalRouter);
+app.use('/user', userRouter);
+app.use('/video', videoRouter);
+
+//init.js
+export default app;
