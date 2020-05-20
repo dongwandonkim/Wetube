4d65dc9364eef3bbc69fdd902a895c66ab9eda38
Dongwan Kim <brkpar@gmail.com>
2020-05-20 03:09:46 -0700

    basic router setup-2

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouter"
-import globalRouter from "./routers/globalRouter"
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
@@ -19,9 +20,9 @@ app.use(morgan("dev"))
 
 
 //Routers
-app.use("/", globalRouter);
-app.use('/user', userRouter);
-app.use('/video', videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 //init.js
 export default app;
