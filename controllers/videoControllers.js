d0b784d5a861f56b8fe95020ba384f4e5c70b062
Dongwan Kim <brkpar@gmail.com>
2020-05-21 02:27:54 -0700

    user Join part functions without db

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,15 +1,17 @@
 import {videos} from "../db"
 
 export const home = (req, res) => {
-
     //videos(fake db.js) send to home.pug
     res.render("home", {pageTitle: "Home", fakedb:videos});
 }
 
 export const search = (req, res) => {
+    const {
+        query: { keyword: searchingBy }
+    } = req;
     //console.log(req.query);
     // keyword === searchbox name
-    res.render("search", {pageTitle: "Search", searchingBy: req.query.keyword});
+    res.render("search", {pageTitle: "Search", searchingBy, videos});
 }
 // export const videos = (req, res) => res.render("videos");
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
