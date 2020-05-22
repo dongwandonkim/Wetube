9bb184b7b10d9cc690dae7fa234d3ba3d5f041bc
Dongwan Kim <brkpar@gmail.com>
2020-05-21 18:52:25 -0700

    db connected

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,8 +1,8 @@
-import {videos} from "../db"
+import routes from "../routes";
 
 export const home = (req, res) => {
     //videos(fake db.js) send to home.pug
-    res.render("home", {pageTitle: "Home", fakedb:videos});
+    res.render("home", {pageTitle: "Home", videos:videos});
 }
 
 export const search = (req, res) => {
@@ -13,8 +13,18 @@ export const search = (req, res) => {
     // keyword === searchbox name
     res.render("search", {pageTitle: "Search", searchingBy, videos});
 }
+
+export const getUpload = (req, res) => 
+    res.render("upload", {pageTitle: "Upload"});
+export const postUpload = (req, res) => {
+    const {
+        body: { file, title, description }
+    } = req;
+    //To do : Upload and save video
+    res.redirect(routes.videoDetail(1212121));
+};
+
 // export const videos = (req, res) => res.render("videos");
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});
-export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
